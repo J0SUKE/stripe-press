16485ad7a7efda6996d3f3607d6212405bdf1198
Jean <chakib3500@gmail.com>
2022-07-07 23:27:22 +0200

    textures

--- components/Shelf/Shelf.js
+++ components/Shelf/Shelf.js
@@ -5,6 +5,7 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { DirectionalLightHelper } from 'three';
 import { throttle } from 'lodash';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+import {DRACOLoader} from 'three/examples/jsm/loaders/DRACOLoader';
 
 export default function Shelf({scrollContainer}) {
   
@@ -58,32 +59,24 @@ function threeCode(container,canvas,scrollContainer) {
   
   //camera
   camera.position.z = 16;
-  camera.position.y = 0;
+  camera.position.y = 1;
   // pour pouvoid 
 
 
   /**
    * Lights
    */
-   const light = new THREE.AmbientLight( 'white',0.5); // soft white light
+   const light = new THREE.AmbientLight( 'white',0.4); // soft white light
    scene.add( light );
 
-   const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
-   //scene.add(new THREE.DirectionalLightHelper(directionalLight,1));
-   
-   directionalLight.position.y = 0;
-   directionalLight.position.z = 2;
+   const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+   //scene.add(new THREE.DirectionalLightHelper(directionalLight,3));
+   directionalLight.lookAt(new THREE.Vector3(0,0,0));
+   directionalLight.position.y = 1;
+   directionalLight.position.z = 3;
    directionalLight.position.x = 0;
    
    scene.add( directionalLight );
-
-
-
-  //meshes
-  let bookGeomerty = new THREE.BoxGeometry(1.4,2.1,0.3);
-
-  let color = ['#afc2e0','#282928','#041b40','#1f2226','#eb4034','#0b1f09','#0a1859','#c7c9d4','#546dde','#282533','#374033'];
-  let BookY =0;
 
   //shelf
   let shelf = new THREE.Group();
@@ -92,41 +85,51 @@ function threeCode(container,canvas,scrollContainer) {
 
    // 3D models ----------------------
 
+  const textureLoader = new THREE.TextureLoader();
+  const book1Texture = textureLoader.load("/models/textures/cover1.png")
+  const book2Texture = textureLoader.load("/models/textures/cover2.png")
+  const book3Texture = textureLoader.load("/models/textures/cover3.png")
+  const book4Texture = textureLoader.load("/models/textures/cover4.png")
+
+  const textures = [book1Texture,book2Texture,book3Texture,book4Texture];
+
   // Instantiate a loader
   const loader = new GLTFLoader();
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath( '/draco/' );
+  loader.setDRACOLoader( dracoLoader );
+
 
   loader.load(
-    '/models/book-cover1.glb',
+    '/models/book.glb',  
     (gltf)=>{
-        gltf.scene.scale.set(7,7,7);
-        gltf.scene.rotateY(-Math.PI/2);
-        shelf.add(gltf.scene);
+        
+        gltf.scene.children[0].scale.set(7,7,7);
+        gltf.scene.children[0].rotateY(-Math.PI/2);      
+
+        const mesh = gltf.scene.children[0];
+        const material = mesh.material;
+
+        let BookY =0;        
+
+
+        textures.forEach(texture=>{
+          const newMaterial = material.clone();
+          if (newMaterial.map) {
+            newMaterial.map = newMaterial.map.clone();
+            newMaterial.map.source = texture.source;
+            newMaterial.needsUpdate = true;
+          }
+          const newMesh = mesh.clone();
+          newMesh.material = newMaterial;
+          newMesh.position.y = BookY;
+          BookY-=0.7;
+          shelf.add(newMesh);
+
+        })
           
         });       
 
-  // color.forEach(color=>{
-  //   let book = new THREE.Mesh(
-  //     bookGeomerty,
-  //     new THREE.MeshStandardMaterial({color})
-  //   )
-
-  //   book.position.y = BookY;
-  //   BookY-=0.6;
-  //   book.rotateZ(Math.PI/2);
-  //   book.rotateY(Math.PI/2);
-  //   shelf.add(book);
-  // })
-
-
-  //handle scroll
-  scrollContainer?.addEventListener('scroll',throttle(()=>{
-    const{scrollTop} = scrollContainer;
-    const {clientHeight} = document.documentElement;
-
-    let wmin = window.innerWidth> clientHeight ? clientHeight : window.innerWidth;
-    shelf.position.y = (scrollTop/wmin)*7;
-
-  },10));
 
   //handle resize
   window.addEventListener('resize', () =>
